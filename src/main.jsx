8635a062a012dee76fff3b328cb72dcecc9af5c7
Mariusm.koder <mariusm.koder@gmail.com>
2024-03-05 09:42:19 +0100

    main jsx experimenting

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+const root = document.getElementById("root")
+const app = ReactDOM.createRoot(root)
+app.render(
+  <h1>Hello World</h1>
+  <button>Press if you DARE</button>
 )
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
