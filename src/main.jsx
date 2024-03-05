fa15a4cc616eea89c2f43449b61ec2610462b757
Mariusm.koder <mariusm.koder@gmail.com>
2024-03-05 10:03:52 +0100

    app jsx testing

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,15 @@ import App from './App.jsx'
 import './index.css'
 
 
-const root = document.getElementById("root")
-const app = ReactDOM.createRoot(root)
-app.render(
-  <h1>Hello World</h1>
-  <button>Press if you DARE</button>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
+
+// const root = document.getElementById("root")
+// const app = ReactDOM.createRoot(root)
+// app.render(
+//   <h1>Hello World</h1>
+//   <button>Press if you DARE</button>
 // )
