0dafae74550a448762c7e9abdb6871d3266a5721
Mariusm.koder <mariusm.koder@gmail.com>
2024-03-11 17:08:27 +0100

    its working

--- src/userCard.jsx
+++ src/userCard.jsx
@@ -1,11 +1,11 @@
 // UserCard.jsx
-import {user} from "./data/users.json";
+import user from "./data/users.json";
 
 function UserCard() {
   return (
     <div className="user-card">
       <h3>{user.users[0].firstName} </h3>
-      {user.users[0].image}
+      <img src={user.users[0].image} alt="Profile Picture" />
       <p>Email: {user.users[0].email}</p>
     </div>
   );
